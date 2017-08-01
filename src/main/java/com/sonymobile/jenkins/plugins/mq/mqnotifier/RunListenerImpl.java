bf38169c35a76aa09caaa04f566bccc554b8f236
TreHack <zexi_yu@126.com>
2017-08-01 11:34:45 +0800

    add starttime in millis

--- src/main/java/com/sonymobile/jenkins/plugins/mq/mqnotifier/RunListenerImpl.java
+++ src/main/java/com/sonymobile/jenkins/plugins/mq/mqnotifier/RunListenerImpl.java
@@ -61,6 +61,7 @@ public class RunListenerImpl extends RunListener<Run> {
         json.put(Util.KEY_URL, Util.getJobUrl(r));
         json.put(Util.KEY_PROJECT_NAME, r.getParent().getFullName());
         json.put(Util.KEY_BUILD_NR, r.getNumber());
+        json.put(Util.KEY_Timestamp, r.getStartTimeInMillis());
         json.put(Util.KEY_MASTER_FQDN, Util.getHostName());
 
         for (MQDataProvider mqDataProvider : MQDataProvider.all()) {
@@ -76,6 +77,7 @@ public class RunListenerImpl extends RunListener<Run> {
         json.put(Util.KEY_URL, Util.getJobUrl(r));
         json.put(Util.KEY_PROJECT_NAME, r.getParent().getFullName());
         json.put(Util.KEY_BUILD_NR, r.getNumber());
+        json.put(Util.KEY_BUILD_DURATION, r.getDuration());
         json.put(Util.KEY_MASTER_FQDN, Util.getHostName());
         String status = "";
         Result res = r.getResult();
